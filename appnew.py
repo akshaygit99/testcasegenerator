36555757371e89db7f2be2246225f4631edd31b2
akshaygit99 <76523662+akshaygit99@users.noreply.github.com>
2024-08-30 18:30:07 +0530

    Update appnew.py

--- appnew.py
+++ appnew.py
@@ -1,60 +1,64 @@
-import streamlit as st
-import openai
-import io
 import os
+import io
+import streamlit as st
 from PIL import Image
 from azure.ai.vision.imageanalysis import ImageAnalysisClient
 from azure.ai.vision.imageanalysis.models import VisualFeatures
 from azure.core.credentials import AzureKeyCredential
 
+try:
+    VISION_KEY = "7f40f7e9afe94109ab10a49cd83e31b5"
+    VISION_ENDPOINT = "https://tcg-cv.cognitiveservices.azure.com/"
+except:
+    print('Missing required env variables')
+    exit()
 
-# Set your Azure OpenAI API key and endpoint
-openai.api_type = "azure"
-openai.api_base = "https://centriinternalgpt.openai.azure.com/"
-openai.api_version = "2024-03-01-preview"
-openai.api_key = "ed16d540ad3f44bba0656e606a943437"
-
-
-# Set your Azure Computer Vision API credentials
-AZURE_CV_KEY = "7f40f7e9afe94109ab10a49cd83e31b5" 
-AZURE_CV_ENDPOINT = "https://tcg-cv.cognitiveservices.azure.com/" 
-
-
-client = ImageAnalysisClient(
-  endpoint = AZURE_CV_ENDPOINT,
-  credential = AzureKeyCredential(AZURE_CV_KEY)
-)
-
-uploaded_image = st.file_uploader("Upload an image to analyze (optional)", type=["jpg", "jpeg", "png"])
-
-if uploaded_image:
-  image = Image.open(uploaded_image)
-  st.image(image, caption='Uploaded Image')
-
-  imageBytes = io.BytesIO()
-  image.save(imageBytes, format=image.format)
-  imageBytes = imageBytes.getvalue()
+try:
+    client = ImageAnalysisClient(
+        endpoint=VISION_ENDPOINT,
+        credential=AzureKeyCredential(VISION_KEY)
+    )
+except:
+    st.error('Cannot connect to Azure Computer Vision')
+    exit()
 
 
-if st.button('Analyse Image'):
-  try:
-      visual_features = [
-        VisualFeatures.TAGS,
-        VisualFeatures.CAPTION,
-        VisualFeatures.DENSECAPTIONS
-      ]
-      result = client.analyze_from_url(
-      image_url="https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png",
-      visual_features=[VisualFeatures.CAPTION, VisualFeatures.READ],
-      gender_neutral_caption=True,  # Optional (default is False)
-      )
-     
-      if result.caption:
-        st.write("Caption:")
-        st.write(f'{result.caption.text}')
-        st.write(f'{result.caption.confidence:.4f}')
-      if len(result.dense_captions.list) >0:
-        st.write('Dense Captions ')
-        st.dataframe(result.dense_captions.list)
-  except:
-      pass
+st.title('Computer Vision with Streamlit')
+
+uploadedFile = st.file_uploader('Choose image', type=['jpg', 'jpeg'])
+
+if uploadedFile:
+    image = Image.open(uploadedFile)
+    st.image(image, caption='Uploaded image')
+
+    imageBytes = io.BytesIO()
+    image.save(imageBytes, format=image.format)
+    imageBytes = imageBytes.getvalue()
+
+    if st.button('Analyze image'):
+        try:
+            visual_features = [
+                VisualFeatures.TAGS,
+                VisualFeatures.CAPTION,
+                VisualFeatures.DENSE_CAPTIONS
+            ]
+
+            result = client.analyze(
+                image_data=imageBytes,
+                visual_features=visual_features
+            )
+
+            if result.caption:
+                st.write("Caption:")
+                st.write(f'{result.caption.text}')
+                st.write(f'{result.caption.confidence:.4f}')
+
+            if len(result.dense_captions.list) > 0:
+                st.write('Dense Captions')
+                st.dataframe(result.dense_captions.list)
+
+            if len(result.tags.list) > 0:
+                st.write('Tags')    
+                st.dataframe(result.tags.list)
+        except Exception as e:
+            st.error(f'There was an error when analysing the image {e}')
