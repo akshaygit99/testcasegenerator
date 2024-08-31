d0da649efd5adba91c0e4b55161718ab126b297d
akshaygit99 <76523662+akshaygit99@users.noreply.github.com>
2024-08-31 18:21:00 +0530

    Update azureapp.py

--- azureapp.py
+++ azureapp.py
@@ -1,12 +1,15 @@
 import streamlit as st
+
 import openai
+
 import os
-from PIL import Image
 
 # Retrieve the API key from the environment variable
+
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Initialize the OpenAI client with the API key
+
 openai.api_key = OPENAI_API_KEY
 
 st.markdown("""
@@ -17,21 +20,23 @@ st.markdown("""
 <p style='font-size: 15px; text-align: left;'>This utility generates detailed software test cases based on user requirements, including BDD format, <b> powered by ChatGPT </b>. It's designed to streamline your testing process and improve efficiency.</p>
 """, unsafe_allow_html=True)
 
-# Define the function to generate test cases
-def generate_test_cases(requirement, uploaded_image=None):
-    # Modify the requirement to include a note about the uploaded image
-    if uploaded_image is not None:
-        combined_requirement = f"{requirement}\n[Note: Please generate test cases based on the provided requirement and the uploaded image.]"
-    else:
-        combined_requirement = requirement
 
-    # Call the OpenAI service to generate test cases
-    response = openai.ChatCompletion.create(
-        model="GPT-4",
+# Define the function to generate test cases
+
+def generate_test_cases(requirement):
+
+    response = openai.chat.completions.create(
+
+        model="gpt-3.5-turbo",
+
         messages=[
-            {"role": "system", "content": "You are a helpful assistant capable of generating software test cases from uploaded Image"},
-            {"role": "user", "content": combined_requirement}
+
+            {"role": "system", "content": "You are a helpful assistant capable of generating software test cases."},
+
+            {"role": "user", "content": requirement}
+
         ]
+
     )
 
     return response.choices[0].message.content
@@ -39,40 +44,45 @@ def generate_test_cases(requirement, uploaded_image=None):
 # Streamlit app layout
 st.title('Test Case Generator :  COE-AI Test')
 
-st.write('Enter your software requirement(s) to generate test cases:')
+st.write('Enter your software requirement(s) to generate test cases :')
 
 if 'search_history' not in st.session_state:
     st.session_state.search_history = []
 
+
 # Text area for user to enter the software requirement
+
+st.link_button("Centric India - AI Tool Usage Policy ", "https://centricconsultingllc.sharepoint.com/sites/CentricIndia/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FCentricIndia%2FShared%20Documents%2FHR%2FCentric%20India%20AI%20Tool%20Usage%20Policy%5F2023%2Epdf&parent=%2Fsites%2FCentricIndia%2FShared%20Documents%2FHR")
+
 requirement = st.text_area("Requirement", height=150)
 
-# Image upload
-st.write('Upload an image:')
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
 # Button to generate test cases
+
 if st.button('Generate Test Cases'):
 
-    if requirement or uploaded_file:
+    if requirement:
 
         with st.spinner('Generating...'):
 
             try:
-                # Generate test cases based on the requirement and uploaded image
-                test_cases = generate_test_cases(requirement, uploaded_image=uploaded_file)
+                
+                test_cases = generate_test_cases(requirement)
 
                 st.success('Generated Test Cases')
+
                 st.write(test_cases)
 
             except Exception as e:
+
                 st.error('An error occurred while generating test cases.')
+
                 st.error(e)
 
             st.session_state.search_history.append(requirement)
 
     else:
-        st.error('Please enter a requirement or upload an image to generate test cases.')
+
+        st.error('Please enter a requirement to generate test cases.')
 
 st.write('Search History:')
 st.write(st.session_state.search_history)
