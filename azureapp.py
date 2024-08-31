b109f9bcabd8373d9adef45e28d79dd9ddc21c3c
akshaygit99 <76523662+akshaygit99@users.noreply.github.com>
2024-08-31 18:39:17 +0530

    Update azureapp.py

--- azureapp.py
+++ azureapp.py
@@ -34,7 +34,7 @@ def generate_test_cases(requirement, uploaded_image):
     # Add text content to the request
     messages.append({
         "role": "user",
-        "content": requirement + img_str
+        "content": requirement
     })
 
     # If an image is uploaded, encode it to Base64 and add it to the content
